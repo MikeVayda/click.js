27fbddbfd43588620ef68b2e63d30bdaa1df3b04
roasted pork <9mvayda@stpatrick.org>
2017-03-24 10:52:27 -0500

    Done

---
+++ click.js-master/click.js
@@ -0,0 +1,16 @@
+//declare samecolor() here
+function samecolor(){
+  let doc = document;
+  let div = doc.querySelector("#samecolor div");
+  for(let i=0;i<#samecolor.length;i++){
+    #samecolor[i].style.color = #1 0 0;
+  }
+}
+//declare diffcolor() here
+function diffcolor(){
+  let doc = document;
+  let div = doc.querySelector("#diffcolor div");
+  for(let i=0;i<#diffcolor.length;i++){
+    #diffcolor[i].style.color = #1 0 1;
+  }
+}
